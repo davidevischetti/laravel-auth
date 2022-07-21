ae3ef53557388b1138a4723cce78b23e771b32d0
davide <davidevischetti@gmail.com>
2022-07-21 22:54:57 +0200

    routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,12 +42,16 @@
                          @endif
                      @else
                          <li class="nav-item dropdown">
-                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                  {{ Auth::user()->name }}
                              </a>
 
                              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
+                                 <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                  document.getElementById('logout-form').submit();">
+                                     {{ __('Logout') }}
+                                 </a>
 
                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                      @csrf
@@ -62,11 +66,6 @@
                 </form> --}}
               </div>
             </div>
-            <a class="dropdown-item" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-             {{ __('Logout') }}
-         </a>
           </nav>
 
 
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('dashboard');
+    Route::resource('posts','PostController');
+    });
