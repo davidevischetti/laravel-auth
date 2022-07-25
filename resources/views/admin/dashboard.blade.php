c354e33e77748c2579c77443fe319f163713d39e
davide <davidevischetti@gmail.com>
2022-07-25 17:12:49 +0200

    add dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<ul class="nav flex-column">
-    <li class="nav-item">
-      <a class="nav-link disabled" aria-current="page" href="#">Dashboard</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{route('admin.posts.index')}}">Posts</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{route('admin.users.index')}}">Users</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Categories</a>
-    </li>
-  </ul>
+
+@include('admin/partials/side-nav')
+
 @endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,25 +3,29 @@
 @section('content')
 <a href="{{url()->previous()}}" class="btn btn-info mb-3 ms-3">Back</a>
 
-<table class="table container">
-    <thead class="table-dark">
-    <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Slug</th>
-        <th scope="col">Title</th>
+<div class="d-flex">
+    @include('admin/partials/side-nav')
 
-    </tr>
-    </thead>
-    <tbody>
-        @foreach ($posts as $post)
+    <table class="table">
+        <thead class="table-primary">
         <tr>
-            <td>{{$post->id}}</td>
-            <td>{{$post->slug}}</td>
-            <td>{{$post->title}}</td>
+            <th scope="col">ID</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Title</th>
+
         </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+            <tr>
+                <td>{{$post->id}}</td>
+                <td>{{$post->slug}}</td>
+                <td>{{$post->title}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
 {{$posts->links()}}
 @endsection
